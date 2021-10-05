9bc596b8b117a7ae2ffc99272542378da15f54ce
WILLIAMTHEWEST1 <90918613+WILLIAMTHEWEST1@users.noreply.github.com>
2021-10-05 11:34:18 -0500

    displays number

--- js/site.js
+++ js/site.js
@@ -3,8 +3,53 @@ function displayHundo() {
     let startValue = 0;
     let endValue = 100;
 
+    //get the numbers from ui
     startValue = document.getElementById("startValue").value;
     endValue = document.getElementById("endValue").value;
 
-    alert("Start Value =" + startValue + "End Value =" + endValue );
+    //covert the input into integers 
+    startValue = parseInt(startValue);
+    endValue = parseInt(endValue);
+
+    //check to see if input is numbers
+    if (Number.isInteger(startValue) && Number.isInteger(endValue)) {
+
+        //if true, get the numbers from the start value to the end value
+        let numbers = generateNumbers(startValue, endValue);
+
+        //display the numbers on the page
+        displayNumbers(numbers)
+
+    } else {
+
+    }
+
+    //alter user
+}
+//generate numbers from startvalue to end value
+function generateNumbers(startValue, endValue) {
+
+    //generate the array
+    let numbers = [];
+    for (let index = startValue; index <= endValue; index++) {
+
+        numbers.push(index);
+    }
+    return numbers;
+}
+
+function displayNumbers(numbers) {
+
+    let startValue = numbers[0];
+    let endIndex = numbers.length;
+    let ptag = document.getElementById("results");
+
+    for (let index = 0; index < endIndex; index++) {
+
+        //const element = array[index];
+
+        let number = numbers[index];
+        ptag.innerHTML += number + " | ";
+
+    }
 }
